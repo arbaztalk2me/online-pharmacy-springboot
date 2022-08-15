21327519dee0087dbe59b194f568b4e75a3ad46f
arbaztalk2me <arbaztalk2me@gmail.com>
2022-08-15 10:26:45 +0530

    added logger and sonar lint

--- src/main/java/com/arbaz/demo/PharmacyApplication.java
+++ src/main/java/com/arbaz/demo/PharmacyApplication.java
@@ -1,13 +1,21 @@
 package com.arbaz.demo;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PharmacyApplication {
-
+	
+	private static final Logger logger = LogManager.getLogger(PharmacyApplication.class); 
 	public static void main(String[] args) {
 		SpringApplication.run(PharmacyApplication.class, args);
+		BasicConfigurator.configure();  
+		  logger.info("Hello world");  
+		  logger.info("we are in logger info mode"); 
+		  
 	}
 
 }
